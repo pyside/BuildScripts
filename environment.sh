845fde36c52c3ffdbd599dd1d66ff63a471560f8
Thomas Perl <m@thp.io>
2010-12-02 14:33:07 +0100

    Do Release builds by default (Debug optional)

--- environment.sh
+++ environment.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+export BUILD_TYPE=Release
+
+# If you want to build a Debug release, make sure to have the
+# debugging version of Python installed and uncomment this line:
+#export BUILD_TYPE=Debug
+
 export PYSIDESANDBOXPATH=$HOME/pkg/pyside-sandbox/
 export PATH=$PYSIDESANDBOXPATH/bin:$PATH
 export PYTHONPATH=$PYSIDESANDBOXPATH/lib/python2.6/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$PYSIDESANDBOXPATH/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$PYSIDESANDBOXPATH/lib/pkgconfig:$PKG_CONFIG_PATH
-# set -DENABLE_ICECC=1 if you're using the icecream distributed compiler
-alias runcmake='cmake .. -DCMAKE_INSTALL_PREFIX=$PYSIDESANDBOXPATH -DCMAKE_BUILD_TYPE=Debug -DENABLE_ICECC=0'
 
