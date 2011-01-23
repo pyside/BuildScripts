3f9144b457acef5187e27be2e8ee90b12b508a00
Thomas Perl <thp@thpinfo.com>
2011-01-23 18:14:04 +0100

    Add support for building on Mac OS X

---
+++ dependencies.osx.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Install build dependencies for Mac OS X using Homebrew
+# Get Homebrew from http://mxcl.github.com/homebrew/
+
+if [ "`which brew`" == "" ]; then
+  cat <<EOF
+
+  The command "brew" was not found on your system. This
+  script assumes that you have Homebrew installed. Would
+  you like to open the Homebrew Installation page in your
+  browser now?
+
+EOF
+  /bin/echo -n "(Y/n) "
+  read yn
+  if [ "$yn" == "" -o "$yn" == "y" -o "$yn" == "Y" ]; then
+    url='https://github.com/mxcl/homebrew/wiki/installation'
+    echo "Opening $url..."
+    open "$url"
+  fi
+  echo "Please restart this script after installing Homebrew."
+  exit 1
+fi
+
+brew install cmake libxml2
+
