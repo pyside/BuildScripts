17b0090d5056d693641392d7a55866244377f621
Thomas Perl <m@thp.io>
2011-03-01 00:34:17 +0100

    Add support for setting $QT_SDK_HOME
    This allows users to build PySide against a
    locally-installed version of the Qt SDK.

--- environment.sh
+++ environment.sh
@@ -13,9 +13,14 @@ export LD_LIBRARY_PATH=$PYSIDESANDBOXPATH/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$PYSIDESANDBOXPATH/lib/pkgconfig:$PKG_CONFIG_PATH
 export DYLD_LIBRARY_PATH=$PYSIDESANDBOXPATH/lib:$DYLD_LIBRARY_PATH
 
-# If you want to use Qt SDK, uncomment those lines:
+# If you want to use Qt SDK, uncomment the following line, or set the
+# environment variable $QT_SDK_HOME in something like your ~/.profile
+
 #QT_SDK_HOME="$HOME/qtsdk-2010.05"
-#export PATH=$QT_SDK_HOME/bin:$QT_SDK_HOME/qt/bin:$PATH
-#export LD_LIBRARY_PATH=$QT_SDK_HOME/lib:$LD_LIBRARY_PATH
-#export QTDIR=$QT_SDK_HOME/qt:$QTDIR
+
+if [ "$QT_SDK_HOME" != "" ]; then
+  export PATH=$QT_SDK_HOME/bin:$QT_SDK_HOME/qt/bin:$PATH
+  export LD_LIBRARY_PATH=$QT_SDK_HOME/lib:$LD_LIBRARY_PATH
+  export QTDIR=$QT_SDK_HOME/qt:$QTDIR
+fi
 
