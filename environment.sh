92b5ba688508638255c35e57f28e6547cc7756bc
Thomas Perl <m@thp.io>
2011-02-28 21:46:19 +0100

    Trailing slash removed; add lib64 + Qt SDK support
    Based on a patch from PySide bug 702.

--- environment.sh
+++ environment.sh
@@ -6,10 +6,16 @@ export BUILD_TYPE=Release
 # debugging version of Python installed and uncomment this line:
 #export BUILD_TYPE=Debug
 
-export PYSIDESANDBOXPATH=$HOME/pkg/pyside-sandbox/
+export PYSIDESANDBOXPATH=$HOME/pkg/pyside-sandbox
 export PATH=$PYSIDESANDBOXPATH/bin:$PATH
-export PYTHONPATH=$PYSIDESANDBOXPATH/lib/python2.6/site-packages:$PYTHONPATH
+export PYTHONPATH=$PYSIDESANDBOXPATH/lib/python2.6/site-packages:$PYSIDESANDBOXPATH/lib64/python2.6/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=$PYSIDESANDBOXPATH/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$PYSIDESANDBOXPATH/lib/pkgconfig:$PKG_CONFIG_PATH
 export DYLD_LIBRARY_PATH=$PYSIDESANDBOXPATH/lib:$DYLD_LIBRARY_PATH
+
+# If you want to use Qt SDK, uncomment those lines:
+#QT_SDK_HOME="$HOME/qtsdk-2010.05"
+#export PATH=$QT_SDK_HOME/bin:$QT_SDK_HOME/qt/bin:$PATH
+#export LD_LIBRARY_PATH=$QT_SDK_HOME/lib:$LD_LIBRARY_PATH
+#export QTDIR=$QT_SDK_HOME/qt:$QTDIR
 
