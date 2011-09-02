ae8c490d90673270dfa92294fb4a49778c5d52e6
Thomas Perl <m@thp.io>
2011-09-02 15:20:19 +0200

    Add QtWebKit dependency for Fedora (bug 996)
    Thanks to Anatoly Techtonik and Jon Nordby for the patch.

--- dependencies.fedora.sh
+++ dependencies.fedora.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Install dependencies for RedHat 6 and Fedora 12 (and above)
 
-yum install cmake qt-devel libxml2-devel libxslt-devel python-devel rpmdevtools gcc gcc-c++ make
+yum install cmake qt-devel qt-webkit-devel libxml2-devel libxslt-devel python-devel rpmdevtools gcc gcc-c++ make
 
