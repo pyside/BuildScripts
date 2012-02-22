ddfbd29d0f8a1cb9c07cec3ec5068b1771cc28ac
Thomas Perl <m@thp.io>
2012-02-22 20:51:28 +0100

    Add qtmobilty-dev to list of dependencies
    This is required to build PySide mobility.

--- dependencies.ubuntu.sh
+++ dependencies.ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Install dependencies for Ubuntu
 
-apt-get install build-essential cmake libqt4-dev libxml2-dev libxslt1-dev python-dev
+apt-get install build-essential cmake libqt4-dev libxml2-dev libxslt1-dev python-dev qtmobility-dev
 
